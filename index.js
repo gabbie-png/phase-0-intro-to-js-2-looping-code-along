c6f67e37bcd764d5f4c25922d1d97a8acf4409d8
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-04-19 23:05:08 -0400

    commit tree 87a41f0a5163f6438de2102f0f8bffda830dbd98

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const name = ["Dylan", "Amy", "Kiki"];
+
+function writeCards(name, eventName){
+    const newEvent = [];
+    for(let i = 0; i < name.length; ++i){
+        
+        newEvent.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+
+    
+    }
+
+    return newEvent;
+}
+
+function countDown(integer){
+    while (integer >=0) {
+        console.log(integer--);
+    }
+}
